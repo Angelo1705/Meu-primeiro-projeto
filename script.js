7f88c41465aa45162834b3ac2a0662bbd5ea4219
Angelo1705 <138524049+Angelo1705@users.noreply.github.com>
2023-07-07 13:06:53 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function togglemode() {
     const img = document.querySelector ("#profile img")
     if(html.classList.contains('light')){
         img.setAttribute('src', './assets/cacto.jpg')
-        alt.setAttribute('cacto com óculos de sol e chapéu de praia na cama de estampa de cacto')
     } else {
         img.setAttribute('src', './assets/Sem título.png')
     }
-}
+    }
